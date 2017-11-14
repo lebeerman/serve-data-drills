93017f8b958b611304280b95add5abbacab24d12
dan <omnidbeerman@gmail.com>
2017-11-14 12:15:58 -0700

    corrected status code assignments.

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(cors());
-
-const env = Object.assign({}, process.env, {PORT: 5000});
 app.set('port', (process.env.PORT || 3000));
-
 const data = [{
-      id: 1,
-      cohortName: "17-01-WD-DP",
-      cohortCode: "g100",
-      numberOfStudents: "28"
-    },{
-      id: 2,
-      cohortName: "17-01-DS-GT",
-      cohortCode: "g105",
-      numberOfStudents: "24"
-    },{
-      id: 3,
-      cohortName: "17-01-WD-PX",
-      cohortCode: "g109",
-      numberOfStudents: "30"
-    },{
-      id: 4,
-      cohortName: "17-01-WD-BD",
-      cohortCode: "g110",
-      numberOfStudents: "29"
-    }
+  id: 1,
+  cohortName: '17-01-WD-DP',
+  cohortCode: 'g100',
+  numberOfStudents: '28'
+},{
+  id: 2,
+  cohortName: '17-01-DS-GT',
+  cohortCode: 'g105',
+  numberOfStudents: '24'
+},{
+  id: 3,
+  cohortName: '17-01-WD-PX',
+  cohortCode: 'g109',
+  numberOfStudents: '30'
+},{
+  id: 4,
+  cohortName: '17-01-WD-BD',
+  cohortCode: 'g110',
+  numberOfStudents: '29'
+}
 ];
 function getID(array, id) {
   for (var i = 0; i < array.length; i++) {
@@ -36,23 +33,23 @@ function getID(array, id) {
   }
   return false;
 }
-app.get("/", function(request, response){
+app.get('/', function(request, response){
   response.json(data);
-})
-app.get("/:id", function(request, response){
+});
+app.get('/:id', function(request, response){
   if (getID(data,request.params.id)){
     response.json(getID(data,request.params.id));
-    response.status = 200;
+    response.status(200);
   }
   else {
-    response.status = 404;
+    response.status(404);
     response.json({
-        error: {
-            message: "No record found!"
-        }
-      });
+      error: {
+        message: 'No record found!'
+      }
+    });
   }
-})
+});
 app.listen(app.get('port'), function() {
   console.log('Node app v1 is running on port', app.get('port'));
 });
